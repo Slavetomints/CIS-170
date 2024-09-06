10ac7adf573030cdf4f069644114dcb69a212393
Daisy Hardwick <161798204+Slavetomints@users.noreply.github.com>
2024-09-06 16:15:27 -0500

    fixed logic and worked on python adaptation

--- chapter_03/leap_year_detector.py
+++ chapter_03/leap_year_detector.py
@@ -2,11 +2,11 @@
 # Class: CIS-170
 # Program: Leap Year Detector Python Program
 
-year = input("Please select a year:\n")
+year = int(input("Please select a year:\n"))
 
 if (year % 100 == 0) and (year % 400 == 0):
   print(f"{year} is a leap year.")
-elif year % 4 == 0:
+elif (year % 4 == 0) and (year % 100 != 0):
   print(f"{year} is a leap year.")
 else:
   print(f"{year} is not a leap year.")
