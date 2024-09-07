c315e75d99131198a166e1cc91e8aafa03c2d2bd
Daisy Hardwick <161798204+Slavetomints@users.noreply.github.com>
2024-09-07 13:55:12 -0500

    wrote java and cleaned up c++

--- chapter_03/LeapYearDetector.cpp
+++ chapter_03/LeapYearDetector.cpp
@@ -12,18 +12,18 @@ int main()
   int year;
 
   // Prompt user for year
-  cout << "Please select a year" << endl;
+  cout << "Please select a year." << endl;
   cin >> year;
 
   // Test to see if year is a leap year and display results
   if (((year % 100 == 0) && (year % 400 ==0)) || ((year % 4 == 0) && (year % 100 != 0)))
   {
-    cout << year << " is a leap year" << endl;
+    cout << year << " is a leap year." << endl;
   }
   else 
   {
-    cout << year << " is not a year year" << endl;
+    cout << year << " is not a year year." << endl;
   }
-  
+
   return 0;
 }
