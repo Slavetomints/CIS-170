c456a252764937fd98dbf040bc5608ff0e7175f9
Slavetomints <161798204+Slavetomints@users.noreply.github.com>
2024-09-07 14:07:39 -0500

    Update leap_year_detector.py

--- chapter_03/leap_year_detector.py
+++ chapter_03/leap_year_detector.py
@@ -2,8 +2,10 @@
 # Class: CIS-170
 # Program: Leap Year Detector Python Program
 
+# Prompt user for year
 year = int(input("Please select a year:\n"))
 
+# Test to see if year is a leap year and display results
 if ((year % 100 == 0) and (year % 400 == 0)) or ((year % 4 == 0) and (year % 100 != 0)):
   print(f"{year} is a leap year.")
 else:
