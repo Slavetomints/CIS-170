5627257f6bae3e515f3848232d7a30f164fdff4c
Daisy Hardwick <161798204+Slavetomints@users.noreply.github.com>
2024-09-07 13:22:57 -0500

    worked on python logic

--- chapter_03/leap_year_detector.py
+++ chapter_03/leap_year_detector.py
@@ -4,9 +4,7 @@
 
 year = int(input("Please select a year:\n"))
 
-if (year % 100 == 0) and (year % 400 == 0):
-  print(f"{year} is a leap year.")
-elif (year % 4 == 0) and (year % 100 != 0):
+if ((year % 100 == 0) and (year % 400 == 0)) or ((year % 4 == 0) and (year % 100 != 0)):
   print(f"{year} is a leap year.")
 else:
   print(f"{year} is not a leap year.")
